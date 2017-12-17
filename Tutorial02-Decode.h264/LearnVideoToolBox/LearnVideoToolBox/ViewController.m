2c8da993644cd53c11abb266870ca207c689cf5d
qinyuelong <qinyuelong.cn@gmail.com>
2017-12-17 17:53:02 +0800

    完成VideoToolBox解码H264 工程 需要真机播放 模拟器会崩溃

--- Tutorial02-Decode.h264/LearnVideoToolBox/LearnVideoToolBox/ViewController.m
+++ Tutorial02-Decode.h264/LearnVideoToolBox/LearnVideoToolBox/ViewController.m
@@ -5,6 +5,7 @@
 //  Created by apple on 2017/12/16.
 //  Copyright © 2017年 apple. All rights reserved.
 //  http://www.jianshu.com/p/da18b979aeec
+//  https://developer.apple.com/videos/play/wwdc2014/513/
 
 #import "ViewController.h"
 #import "LYOpenGLView.h"
@@ -90,7 +91,14 @@ const uint8_t lyStartCode[4] = {0,0,0,1};
 }
 
 -(void)onInputEnd{
-    
+    [inputStream close];
+    inputStream = nil;
+    if (inputBuffer) {
+        free(inputBuffer);
+        inputBuffer = NULL;
+    }
+    [self.mDispalyLink setPaused:YES];
+    self.mButton.hidden = NO;
 }
 
 #pragma mark - updateFrame
@@ -107,18 +115,38 @@ const uint8_t lyStartCode[4] = {0,0,0,1};
             *pNalSize = CFSwapInt32HostToBig(nalSize);
             
             // 在buffer的前面填入代表长度的int
-            CVPixelBufferRef piexlBuffer = NULL;
-            int nalType = packetBuffer[4] & 0x1f;
+            CVPixelBufferRef pixelBuffer = NULL;
+            int nalType = packetBuffer[4] & 0x1F;
             switch (nalType) {
                 case 0x05:
                     NSLog(@"Nal type is IDR frame");
                     [self initVideoToolBox];
-                    piexlBuffer = [self decode];
+                    pixelBuffer = [self decode];
                     break;
-                    
+                case 0x07:
+                    NSLog(@"Nal type is SPS");
+                    mSPSSize = packetSize - 4;
+                    mSPS = malloc(mSPSSize);
+                    memcpy(mSPS, packetBuffer + 4, mSPSSize);
+                    break;
+                case 0x08:
+                    NSLog(@"Nal type is PPS");
+                    mPPSSize = packetSize - 4;
+                    mPPS = malloc(mPPSSize);
+                    memcpy(mPPS, packetBuffer + 4, mPPSSize);
+                    break;
                 default:
+                    NSLog(@"Nal type is B/P frame");
+                    pixelBuffer = [self decode];
                     break;
             }
+            if (pixelBuffer) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self.mOpenGLView displayPixelBuffer:pixelBuffer];
+                    CVPixelBufferRelease(pixelBuffer);
+                });
+            }
+            NSLog(@"Read Nalu size %ld", packetSize);
         });
     }
 }
@@ -126,14 +154,146 @@ const uint8_t lyStartCode[4] = {0,0,0,1};
 
 
 -(void)readPacket{
+    if (packetSize && packetBuffer) {
+        packetSize = 0;
+        free(packetBuffer);
+        packetBuffer = NULL;
+    }
+    if (inputSize < inputMaxSize && inputStream.hasBytesAvailable) {
+        inputSize += [inputStream read:inputBuffer + inputSize maxLength:inputMaxSize - inputSize];
+    }
+    if (memcmp(inputBuffer, lyStartCode, 4) == 0) {
+        if (inputSize > 4) {
+            // 除了开始码还有内容
+            uint8_t *pStart = inputBuffer + 4;
+            uint8_t *pEnd = inputBuffer + inputSize;
+            while (pStart != pEnd) {  //这里使用一种简略的方式来获取这一帧的长度：通过查找下一个0x00000001来确定。
+                if (memcmp(pStart - 3, lyStartCode, 4) == 0) {
+                    packetSize = pStart - inputBuffer - 3;
+                    if (packetBuffer) {
+                        free(packetBuffer);
+                        packetBuffer = NULL;
+                    }
+                    packetBuffer = malloc(packetSize);
+                    memcpy(packetBuffer, inputBuffer, packetSize); //复制packet内容到新的缓冲区
+                    memmove(inputBuffer, inputBuffer + packetSize, inputSize - packetSize); //把缓冲区前移
+                    inputSize -= packetSize;
+                    break;
+                }else{
+                    ++pStart;
+                }
+            }
+        }
+    }
     
 }
 
 -(void)initVideoToolBox {
-    
+    if (!mDecodeSession) {
+        const uint8_t *parameterSetPointers[2] = {mSPS, mPPS};
+        const size_t parameterSetSizes[2] = {mSPSSize, mPPSSize};
+        OSStatus status = CMVideoFormatDescriptionCreateFromH264ParameterSets(kCFAllocatorDefault,
+                                                                              2, //param count
+                                                                              parameterSetPointers,
+                                                                              parameterSetSizes,
+                                                                              4, //nal start code size
+                                                                              &mFormatDescription);
+        if (status == noErr) {
+            CFDictionaryRef attrs = NULL;
+            const void *keys[] = {kCVPixelBufferPixelFormatTypeKey};
+            //      kCVPixelFormatType_420YpCbCr8Planar is YUV420
+            //      kCVPixelFormatType_420YpCbCr8BiPlanarFullRange is NV12
+            uint32_t v = kCVPixelFormatType_420YpCbCr8BiPlanarFullRange;
+            const void *values[] = {CFNumberCreate(NULL, kCFNumberSInt32Type, &v)};
+            attrs = CFDictionaryCreate(NULL, keys, values, 1, NULL, NULL);
+            
+            VTDecompressionOutputCallbackRecord callBackRecord;
+            callBackRecord.decompressionOutputCallback = didDecompress;
+            callBackRecord.decompressionOutputRefCon = NULL;
+            
+            status = VTDecompressionSessionCreate(kCFAllocatorDefault,
+                                                  mFormatDescription,
+                                                  NULL,
+                                                  attrs,
+                                                  &callBackRecord,
+                                                  &mDecodeSession);
+            CFRelease(attrs);
+        }else{
+            NSLog(@"IOS8VT: reset decoder session failed status=%d", status);
+        }
+    }
 }
 
--(CVPixelBufferRef)decode{
-    return nil;
+void didDecompress(void *decompressionOutputRefCon, void *sourceFrameRefCon, OSStatus status, VTDecodeInfoFlags infoFlags, CVImageBufferRef pixelBuffer, CMTime presentationTimeStamp, CMTime presentationDuration ){
+    
+    CVPixelBufferRef *outputPixelBuffer = (CVPixelBufferRef *)sourceFrameRefCon;
+    *outputPixelBuffer = CVPixelBufferRetain(pixelBuffer);
 }
+
+
+-(CVPixelBufferRef)decode{
+    CVPixelBufferRef outputPixelBuffer = NULL;
+    if (mDecodeSession) {
+        CMBlockBufferRef blockBuffer = NULL;
+        OSStatus status = CMBlockBufferCreateWithMemoryBlock(kCFAllocatorDefault,
+                                                    (void *)packetBuffer, packetSize,
+                                                    kCFAllocatorNull,
+                                                    NULL, 0, packetSize,
+                                                    0, &blockBuffer);
+        if (status == kCMBlockBufferNoErr) {
+            CMSampleBufferRef sampleBuffer = NULL;
+            const size_t sampleSizeArray[] = {packetSize};
+            status = CMSampleBufferCreateReady(kCFAllocatorDefault,
+                                               blockBuffer,
+                                               mFormatDescription,
+                                               1, 0, NULL, 1, sampleSizeArray,
+                                               &sampleBuffer);
+            if (status == kCMBlockBufferNoErr && sampleBuffer) {
+                VTDecodeFrameFlags flags = 0;
+                VTDecodeInfoFlags flagOut = 0;
+                // 默认是同步操作。
+                // 调用didDecompress，返回后再回调
+                OSStatus decodeStatus = VTDecompressionSessionDecodeFrame(mDecodeSession,
+                                                                          sampleBuffer,
+                                                                          flags,
+                                                                          &outputPixelBuffer,
+                                                                          &flagOut);
+                if (decodeStatus == kVTInvalidSessionErr) {
+                    NSLog(@"IOS8VT: Invalid session, reset decoder session");
+                }else if (decodeStatus == kVTVideoDecoderBadDataErr) {
+                    NSLog(@"IOS8VT: decode failed status=%d(Bad data)", decodeStatus);
+                }else if (decodeStatus != noErr) {
+                    NSLog(@"IOS8VT: decode failed status=%d", decodeStatus);
+                }
+                CFRelease(sampleBuffer);
+            }
+            CFRelease(blockBuffer);
+        }
+    }
+    return outputPixelBuffer;
+}
+
+
+
+-(void)EndVideoToolBox{
+    if (mDecodeSession) {
+        VTDecompressionSessionInvalidate(mDecodeSession);
+        CFRelease(mDecodeSession);
+        mDecodeSession = NULL;
+    }
+    if (mFormatDescription) {
+        CFRelease(mFormatDescription);
+        mFormatDescription = NULL;
+    }
+    
+    free(mSPS);
+    free(mPPS);
+    mPPSSize = 0;
+}
+
+
+-(void)dealloc{
+    [self EndVideoToolBox];
+}
+
 @end
